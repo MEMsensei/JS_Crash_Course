e60abe8e85047e2d14a81b17369b4321f3e26831
MEMsensei <47557681+MEMsensei@users.noreply.github.com>
2023-03-14 23:28:36 +0200

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,29 @@
+// 1
+// const userBirthYear = prompt("Enter your birth year",""),
+//       currentYear = 2023;
+// alert(`You are ${currentYear-userBirthYear} years old`);
+
+
+// 2
+// const radius = prompt("Enter the radius of the circle: ","");
+// alert(`Area of the circle is: ${3.1415*radius*radius}`);
+
+
+// 3
+// const distance = prompt("Enter the distance between two cities: ", ""),
+//       timeToReach = prompt("Enter the time you need to pass the distance(in minutes): ");
+// alert(`You need to have ${distance/timeToReach * 60} km/h velocity to reach the desstination on time `)
+
+
+// 4
+// const usdToeuro = 1.07,
+//       usdAmount = prompt("Enter amount of dollars to change: ", "");
+// alert(`${usdAmount} usd = ${usdAmount*usdToeuro} euro`);
+
+
+// 5
+// const gigaBytes = prompt("Enter amount of Gb in your flash drive: ",""),
+//       fileSize = 820;
+// alert(`${Math.floor(gigaBytes*1024/820)} files can be saved on your flash drive`);
+
+//did't make the validation of input value because it wasn't in the task
